50deef69e8809d1b83a54ef67152c98d384a9858
greggTime <gregg.rybicki@gmail.com>
2022-08-17 19:53:13 -0700

    Adding a type in a hacky way for now

---
+++ types/incident-object.ts
@@ -0,0 +1,12 @@
+export type Incident = {
+  short_description: string;
+  severity: string;
+  long_description?: string;
+  incident_participants?: string | undefined;
+  incident_dri?: string | undefined;
+  incident_start_time: string | undefined;
+  incident_trigger?: string;
+  incident_id?: string;
+  external_incident_id?: string;
+  slack_reporter?: string | undefined;
+};
