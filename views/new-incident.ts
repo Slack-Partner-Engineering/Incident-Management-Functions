b3e572596f3833d0c4254b1f44c6871281fe73d6
greggTime <gregg.rybicki@gmail.com>
2022-08-17 17:44:30 -0700

    changing to any and ignoring for now, add types another day

--- views/new-incident.ts
+++ views/new-incident.ts
@@ -1,11 +1,6 @@
-const newIncident = (
-  incidentObject: {
-    incident_trigger: any;
-    short_description: any;
-    incident_participants: any;
-    incident_dri: any;
-  },
-) => { //anoying type workaround, guess I will have to add the incident object as a type, creating a jira.
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// deno-lint-ignore-file
+const newIncident = (incidentObject: any) => { //anoying type workaround, guess I will have to add the incident object as a type, creating a jira.
   const blocks = [
     {
       type: "section",
