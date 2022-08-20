cd349cbd7b83d3d1c2047949fb0e8cdc95599ab4
greggTime <gregg.rybicki@gmail.com>
2022-08-19 17:11:57 -0700

    adding DB files

--- utils/database/update-incident.ts
+++ utils/database/update-incident.ts
@@ -1,1 +1,29 @@
 //  await saveAuditRecord(token, incident);
+import type { Incident } from "../../types/incident-object.ts";
+import { SlackAPI } from "deno-slack-api/mod.ts";
+import { saveAuditRecord } from "./save-audit.ts";
+
+const updateIncident = async (token: string, incident: Incident) => {
+  const client = SlackAPI(token, {});
+
+  await saveAuditRecord(token, incident);
+
+  const response = await client.apps.datastore.put(
+    {
+      datastore: "Incidents",
+      item: incident,
+    },
+  );
+
+  if (!response.ok) {
+    console.log("Error calling apps.datastore.put:");
+    return {
+      error: response.error,
+    };
+  } else {
+    console.log("Datastore put successful!");
+    return response.item;
+  }
+};
+
+export { updateIncident };
