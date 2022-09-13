d1fd88e258925885f3362d8712ac0fa4bf05ed12
greggTime <gregg.rybicki@gmail.com>
2022-09-13 14:32:31 -0700

    Added some percentage fields to the report

--- utils/report/new-incident-report.ts
+++ utils/report/new-incident-report.ts
@@ -9,6 +9,8 @@ const generateNewIncidentReport = async (token: string) => {
   report.numberOfOpenIncidents = 0;
   report.numberOfClosedIncidents = 0;
   report.incidentDris = {};
+  report.incidentStatusCount = {};
+  report.incidentSeverity = {};
 
   for (let i = 0; i < report.items.length; i++) {
     const element = <Incident> report.items[i];
@@ -16,13 +18,31 @@ const generateNewIncidentReport = async (token: string) => {
     if (element.incident_swarming_msg_ts) {
       report.numberOfSwarmingIncidents++;
     }
+
     if (element.incident_status === "OPEN") {
       report.numberOfOpenIncidents++;
     } else {
       report.numberOfClosedIncidents++;
     }
+
     if (element.incident_dri) {
       report.incidentDris[element.incident_dri] = "";
+    }
+
+    if (element.incident_status) {
+      if (report.incidentStatusCount[element.incident_status]) {
+        report.incidentStatusCount[element.incident_status]++;
+      } else {
+        report.incidentStatusCount[element.incident_status] = 1;
+      }
+    }
+
+    if (element.severity) {
+      if (report.incidentSeverity[element.severity]) {
+        report.incidentSeverity[element.severity]++;
+      } else {
+        report.incidentSeverity[element.severity] = 1;
+      }
     }
   }
 
--- views/new-incident-report.ts
+++ views/new-incident-report.ts
@@ -1,13 +1,29 @@
 const newIncidentReport = (incidentReport: any) => {
   let reportText = "";
 
+  console.log(incidentReport.incidentSeverity);
+
   let incidentDRIs = "";
   for (const key in incidentReport.incidentDris) {
     if (
       Object.prototype.hasOwnProperty.call(incidentReport.incidentDris, key)
     ) {
       incidentDRIs += `<@${key}>`;
-      console.log(key);
+    }
+  }
+
+  let incidentSeveritiesPercentage = "";
+  for (const key in incidentReport.incidentSeverity) {
+    if (
+      Object.prototype.hasOwnProperty.call(incidentReport.incidentSeverity, key)
+    ) {
+      incidentSeveritiesPercentage += `${key}: ${
+        Math.round(
+          (incidentReport.incidentSeverity[key] /
+            incidentReport.numberOfIncidents) * 100,
+        )
+      }% \n`;
+      console.log(incidentReport.incidentSeverity[key]);
     }
   }
 
@@ -16,7 +32,18 @@ const newIncidentReport = (incidentReport: any) => {
     .concat(`Swarming Incidents: ${incidentReport.numberOfSwarmingIncidents}\n`)
     .concat(`Open Incidents: ${incidentReport.numberOfOpenIncidents} \n`)
     .concat(`Closed Incidents: ${incidentReport.numberOfClosedIncidents}\n`)
-    .concat(`Incident DRIs: ${incidentDRIs}`);
+    .concat(`Incident DRIs: ${incidentDRIs}\n\n`)
+    .concat(
+      `Percentage of OPEN Incidents: ${
+        Math.round(
+          (incidentReport.incidentStatusCount.OPEN /
+            incidentReport.numberOfIncidents) * 100,
+        )
+      }%\n\n`,
+    )
+    .concat(
+      `Percentage of Incidents by Severity: \n${incidentSeveritiesPercentage}`,
+    );
 
   const blocks: any = [
     {
