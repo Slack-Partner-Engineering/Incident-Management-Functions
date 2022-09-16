b08e667ed4064e26a19cb4fe7521ebc62eacaaf3
Horea Porutiu <hporutiu@slack-corp.com>
2022-09-16 11:49:04 -0400

    adding assign DRI logic

---
+++ functions/assign_dri/assignDRI.ts
@@ -0,0 +1,32 @@
+//This function should take in the standard incident object from various triggers.
+//It should then create the incident channel, etc... to kick off the process.
+//input: standard incident object
+//output: channel URL, channel_id, incident object.
+//
+import { postMessage } from "../../utils/slack_apis/post-message.ts";
+import { postReply } from "../../utils/slack_apis/post-message.ts";
+import { updateMessage } from "../../utils/slack_apis/update-message.ts";
+import { Incident } from "../../types/incident-object.ts";
+import { updateIncident } from "../../utils/database/update-incident.ts";
+import { getIncident } from "../../utils/database/get-incident.ts";
+import { newIncident } from "../../views/new-incident.ts";
+import { incidentHandler } from "../../utils/blockActionHandlers/incident-buttons-handler.ts";
+import { assignDRIModal } from "../../views/assing-dri-modal.ts";
+import { openView } from "../../utils/slack_apis/open-view.ts";
+
+export const assignDRI = async (
+  incident: Incident,
+  env: any,
+  token: any,
+  body: any,
+) => {
+  const curIncident = await getIncident(token, <string> incident.incident_id);
+  const incidentStr = await JSON.stringify(incident);
+  const driModal = await assignDRIModal(incidentStr);
+  await openView(
+    token,
+    driModal,
+    body.interactivity.interactivity_pointer,
+  );
+  console.log("after open view");
+};
