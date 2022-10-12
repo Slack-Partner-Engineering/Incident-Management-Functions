d4409408c013acb6748c5a0e725d01c2de51f3ba
greggTime <gregg.rybicki@gmail.com>
2022-10-12 16:16:21 -0700

    patch sms

--- utils/externalAPIs/clerk/messages/new-incident-message.ts
+++ utils/externalAPIs/clerk/messages/new-incident-message.ts
@@ -1,8 +1,10 @@
 import { Incident } from "../../../../types/incident-object.ts";
 
 const newIncidentClerk = (incident: Incident) => {
+  console.log(incident.short_description);
+
   const message =
-    `A new Critical incident has been created.   Incident Id: ${incident.incident_id}.  ${incident.short_description}`;
+    `A new Critical incident has been created.   Incident Id: ${incident.incident_id}.      ${incident.short_description}           ${incident.long_description}`;
   return message;
 };
 
